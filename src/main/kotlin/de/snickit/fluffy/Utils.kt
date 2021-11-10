831fb34d35d7b67bb75812cf32aa1accfc3a3f66
Niels Korschinsky <niels.korschinsky@mni.thm.de>
2021-11-10 20:10:20 +0100

    Add comments and changes notation of getSemesterForDate function

--- src/main/kotlin/de/snickit/fluffy/Utils.kt
+++ src/main/kotlin/de/snickit/fluffy/Utils.kt
@@ -5,30 +5,56 @@ import net.dv8tion.jda.api.entities.GuildChannel
 import net.dv8tion.jda.api.entities.Member
 import org.slf4j.LoggerFactory
 import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 object Utils {
 
     private val logger = LoggerFactory.getLogger(this::class.java)
 
+    /**
+     *01.04.XX, start des Sommersemesters
+     */
     private val summerStartDay: LocalDateTime = LocalDateTime.of(0, 4, 1, 0, 0)
 
+    /**
+     *1.10, start des Wintersemesters
+     */
     private val  winterStartDay: LocalDateTime = LocalDateTime.of(0, 10, 1, 0, 0)
 
-    private const val gracePeriodInDays: Long = 30
+    /**
+     * Gibt an, in welcher Zeitspanne nach Semesterstart Module abgeschlossen werden können, damit Sie noch zum vorherigen Semester zählen.
+     * Nötig falls z.B Blockkurse über den Semesterstart hinausgehen.
+     */
+    private const val gracePeriodInDays: Long = 60
 
+    /**
+     * Gibt das Semester für ein bestimmtes Datum an.
+     * Notation:
+     * - 20-21ws → Wintersemester 2020/2021
+     * - 21ss → Sommersemester 2021
+     * @param date Datum zu überprüfen
+     * @return YY-YYws oder YYss, z.B 21-22ws
+     */
     private fun getSemesterForDate(date: LocalDateTime): String {
         val summerSameYear = summerStartDay.withYear(date.year).plusDays(gracePeriodInDays)
-
+        val dateFormat = DateTimeFormatter.ofPattern("yy")
+        val thisYear = dateFormat.format(date)
+        //vor dem z.B 01.04 des Jahres -> WS letztes/dieses Jahr
         if (date.isBefore(summerSameYear)) {
-            return "${date.year}WS"
+            //21-22ws
+            return "${dateFormat.format(date.minusYears(1))}-${thisYear}ws"
         }
 
         val winterSameYear = winterStartDay.withYear(date.year).plusDays(gracePeriodInDays)
+
+        // Alles vor winterSameYear wird zum vorherigen Sommersemester gezählt
         if (date.isBefore(winterSameYear)) {
-            return "${date.year}SS"
+            return "${thisYear}ss"
         }
 
-        return "${date.year + 1}WS"
+        // Nach dem SS zählt alles ins nächste WS dieses/nächstes Jahr
+        //22-23ws
+        return "${thisYear}-${dateFormat.format(date.plusYears(1))}ws"
     }
 
     fun getCurrentSemester(): String =
